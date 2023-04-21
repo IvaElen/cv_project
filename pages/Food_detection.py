87bf9faf3cc4e919e0d90c5152018119a60c39d1
Elena Ivanova <123091556+IvaElen@users.noreply.github.com>
2023-04-21 12:16:27 +0300

    Update Food_detection.py

--- pages/Food_detection.py
+++ pages/Food_detection.py
@@ -3,7 +3,6 @@ import streamlit as st
 import wget
 from PIL import Image
 import torch
-import cv2
 import os
 import time
 
@@ -30,59 +29,59 @@ def image_input(data_src):
             st.image(img, caption="Model prediction")
 
 
-def video_input(data_src):
-    vid_file = None
-    if data_src == 'Sample data':
-        vid_file = "pages/data/sample_videos/sample.mp4"
-    else:
-        vid_bytes = st.sidebar.file_uploader("Upload a video", type=['mp4', 'mpv', 'avi'])
-        if vid_bytes:
-            vid_file = "pages/data/uploaded_data/upload." + vid_bytes.name.split('.')[-1]
-            with open(vid_file, 'wb') as out:
-                out.write(vid_bytes.read())
+# def video_input(data_src):
+#     vid_file = None
+#     if data_src == 'Sample data':
+#         vid_file = "pages/data/sample_videos/sample.mp4"
+#     else:
+#         vid_bytes = st.sidebar.file_uploader("Upload a video", type=['mp4', 'mpv', 'avi'])
+#         if vid_bytes:
+#             vid_file = "pages/data/uploaded_data/upload." + vid_bytes.name.split('.')[-1]
+#             with open(vid_file, 'wb') as out:
+#                 out.write(vid_bytes.read())
 
-    if vid_file:
-        cap = cv2.VideoCapture(vid_file)
-        custom_size = st.sidebar.checkbox("Custom frame size")
-        width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-        height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        if custom_size:
-            width = st.sidebar.number_input("Width", min_value=120, step=20, value=width)
-            height = st.sidebar.number_input("Height", min_value=120, step=20, value=height)
+#     if vid_file:
+#         cap = cv2.VideoCapture(vid_file)
+#         custom_size = st.sidebar.checkbox("Custom frame size")
+#         width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+#         height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+#         if custom_size:
+#             width = st.sidebar.number_input("Width", min_value=120, step=20, value=width)
+#             height = st.sidebar.number_input("Height", min_value=120, step=20, value=height)
 
-        fps = 0
-        st1, st2, st3 = st.columns(3)
-        with st1:
-            st.markdown("## Height")
-            st1_text = st.markdown(f"{height}")
-        with st2:
-            st.markdown("## Width")
-            st2_text = st.markdown(f"{width}")
-        with st3:
-            st.markdown("## FPS")
-            st3_text = st.markdown(f"{fps}")
+#         fps = 0
+#         st1, st2, st3 = st.columns(3)
+#         with st1:
+#             st.markdown("## Height")
+#             st1_text = st.markdown(f"{height}")
+#         with st2:
+#             st.markdown("## Width")
+#             st2_text = st.markdown(f"{width}")
+#         with st3:
+#             st.markdown("## FPS")
+#             st3_text = st.markdown(f"{fps}")
 
-        st.markdown("---")
-        output = st.empty()
-        prev_time = 0
-        curr_time = 0
-        while True:
-            ret, frame = cap.read()
-            if not ret:
-                st.write("Can't read frame, stream ended? Exiting ....")
-                break
-            frame = cv2.resize(frame, (width, height))
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            output_img = infer_image(frame)
-            output.image(output_img)
-            curr_time = time.time()
-            fps = 1 / (curr_time - prev_time)
-            prev_time = curr_time
-            st1_text.markdown(f"**{height}**")
-            st2_text.markdown(f"**{width}**")
-            st3_text.markdown(f"**{fps:.2f}**")
+#         st.markdown("---")
+#         output = st.empty()
+#         prev_time = 0
+#         curr_time = 0
+#         while True:
+#             ret, frame = cap.read()
+#             if not ret:
+#                 st.write("Can't read frame, stream ended? Exiting ....")
+#                 break
+#             frame = cv2.resize(frame, (width, height))
+#             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+#             output_img = infer_image(frame)
+#             output.image(output_img)
+#             curr_time = time.time()
+#             fps = 1 / (curr_time - prev_time)
+#             prev_time = curr_time
+#             st1_text.markdown(f"**{height}**")
+#             st2_text.markdown(f"**{width}**")
+#             st3_text.markdown(f"**{fps:.2f}**")
 
-        cap.release()
+#         cap.release()
 
 
 def infer_image(img, size=None):
@@ -153,14 +152,14 @@ def main():
         st.sidebar.markdown("---")
 
         # input options
-        input_option = st.sidebar.radio("Select input type: ", ['image', 'video'])
+#         input_option = st.sidebar.radio("Select input type: ", ['image', 'video'])
 
         # input src option
         data_src = 'Upload your own data'
-        if input_option == 'image':
-            image_input(data_src)
-        else:
-            video_input(data_src)
+        input_option = 'image'
+        image_input(data_src)
+#         else:
+#             video_input(data_src)
 
 
 if __name__ == "__main__":
